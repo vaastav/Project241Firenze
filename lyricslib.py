11fc4776239cb4564b7a1e35cd1c15fc28b9b977
vaastav <vaastav.anand05@gmail.com>
2016-02-27 17:09:08 -0800

    Add a compare function that compares the lyrics of the song

--- lyricslib.py
+++ lyricslib.py
@@ -1,12 +1,37 @@
-from urllib.parse import urlparse
 
-def clean_string( s ):
-	t = s.strip().replace(' ','').lower()
+def clean_string( s , keepSpaces = False):
+	if keepSpaces:
+		t = s.strip().lower()
+	else:
+		t = s.strip().replace(' ','').lower()
 	return t
 
 def lyrics( artist, song ):
-	baseURL = 'www.azlyrics.com/lyrics/'
+	baseURL = 'http://www.azlyrics.com/lyrics/'
 	cleanedSong = clean_string( song )
 	cleanedArtist = clean_string( artist )
 	lyricsURL = baseURL + cleanedArtist + '/' + cleanedSong + '.html'
-	print( lyricsURL )
+	return lyricsURL 
+
+def compare( lyricsFile1, lyricsFile2 ):
+	num_target = 0
+	num_source = 0
+	
+	l1 = open( lyricsFile1, 'rU')
+	l2 = open( lyricsFile2, 'rU')
+	target = clean_string( l1.read(),True )
+	source = clean_string( l2.read(),True )
+	length = len( source.replace('\n',' ').split(' ') )
+	word_counter = [False] * length
+	for t in target.replace('\n',' ').split(' '):
+		num_source = 0
+		for s in source.replace('\n',' ').split(' '):
+			if t == s :
+				if word_counter[num_source] == False:
+					word_counter[num_source] = True
+					break
+			num_source += 1
+		num_target += 1
+	return float( sum(word_counter)/num_target ) * 100.0
+	
+			
