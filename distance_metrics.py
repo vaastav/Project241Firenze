fdf019524ef223e8310cda9290952da7e342169d
Sonalee Shah <sssonalee@gmail.com>
2016-02-28 05:33:32 -0800

    Calculating conditional Euclidean distance

---
+++ distance_metrics.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 28 05:07:39 2016
+
+@author: sssonalee
+"""
+from scipy.spatial.distance import hamming
+from scipy.spatial.distance import euclidean
+import numpy as np
+
+def conditional_euclidean_distance (values1, values2):
+    return euclidean(values1, values2)/hamming(values1, values2)
+
+#arr = np.array([1, 1.6, 2.4, 3.6, 5.2, 6.6, 8, 9.2, 10])
+#arr2 = np.array([1, 2, 3, 4, 5, 6, 8, 9, 10])
+#print (conditional_euclidean_distance(arr, arr2))
