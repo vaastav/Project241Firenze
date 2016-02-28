f6659ea0d508be8a06ac67387a0143608dd735f3
danagregg <danagregg@hotmail.com>
2016-02-27 17:52:41 -0800

    buttons open file dialog

--- inheritWindow.py
+++ inheritWindow.py
@@ -6,7 +6,7 @@ class Window(QtGui.QMainWindow):
 	
 	def __init__(self):
 		super(Window,self).__init__()
-		self.setGeometry(50,50,700,400)
+		self.setGeometry(50,50,700,300)
 		self.setWindowTitle("Project Firenze")
 		self.setWindowIcon(QtGui.QIcon("Images/archerlogo.png"))
 		self.home()
@@ -14,16 +14,20 @@ class Window(QtGui.QMainWindow):
 
 	def home(self):
 		firstFile = QtGui.QPushButton("Attach First Song", self)
-		firstFile.clicked.connect(QtCore.QCoreApplication.instance().quit)
+		firstFile.clicked.connect(self.openFile)
 		firstFile.resize(150,50)
 		firstFile.move(100,100)
 
 		secondFile = QtGui.QPushButton("Attach Second Song", self)
-		secondFile.clicked.connect(QtCore.QCoreApplication.instance().quit)
+		secondFile.clicked.connect(self.openFile)
 		secondFile.resize(150,50)
 		secondFile.move(450,100)
 
 		self.show()
+
+	def openFile(self):
+		self.fileDialog = QtGui.QFileDialog(self)
+		self.fileDialog.show()
 
 
 
