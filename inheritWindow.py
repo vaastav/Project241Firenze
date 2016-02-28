f68e43a86211c88cc65e5a69eb69165c0d35a28a
danagregg <danagregg@hotmail.com>
2016-02-27 20:51:53 -0800

    Added labels

--- inheritWindow.py
+++ inheritWindow.py
@@ -6,7 +6,7 @@ class Window(QtGui.QMainWindow):
 	
 	def __init__(self):
 		super(Window,self).__init__()
-		self.setGeometry(50,50,700,300)
+		self.setGeometry(50,50,1300,500)
 		self.setWindowTitle("Project Firenze")
 		self.setWindowIcon(QtGui.QIcon("Images/archerlogo.png"))
 		self.home()
@@ -14,14 +14,20 @@ class Window(QtGui.QMainWindow):
 
 	def home(self):
 		firstFile = QtGui.QPushButton("Attach First Song", self)
-		firstFile.clicked.connect(self.openFile)
+		firstFile.clicked.connect(self.openFirstFile)
 		firstFile.resize(150,50)
 		firstFile.move(100,100)
 
+		self.firstFileLabel = QtGui.QLabel("First Song", self)
+		self.firstFileLabel.setGeometry(100, 50, 500, 50)
+
 		secondFile = QtGui.QPushButton("Attach Second Song", self)
-		secondFile.clicked.connect(self.openFile)
+		secondFile.clicked.connect(self.openSecondFile)
 		secondFile.resize(150,50)
-		secondFile.move(450,100)
+		secondFile.move(100,300)
+
+		self.secondFileLabel = QtGui.QLabel("Second Song", self)
+		self.secondFileLabel.setGeometry(100, 250, 500, 50)
 
 		compareSongs = QtGui.QPushButton("Compare Songs", self)
 		compareSongs.clicked.connect(self.download)
@@ -32,19 +38,25 @@ class Window(QtGui.QMainWindow):
 		self.progress.setGeometry(245, 175, 250, 20)
 
 
-		
-
-		# lbl = QtGui.QLabel('No file selected')
-		# self.addWidget(self.lbl)
 
 		self.show()
 
-	def openFile(self):
+	def openFirstFile(self):
 		# self.fileDialog = QtGui.QFileDialog(self)
 		# self.fileDialog.show()
 
 		name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
 		print(name)
+		self.firstFileLabel.setText(name)
+
+		file = open(name, 'r')
+
+	def openSecondFile(self):
+
+		name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
+		print(name)
+		self.secondFileLabel.setText(name)
+
 		file = open(name, 'r')
 
 	def download(self):
@@ -52,30 +64,6 @@ class Window(QtGui.QMainWindow):
 		while self.completed < 100:
 			self.completed+= 0.001
 			self.progress.setValue(self.completed)
-
-    #     # Create a label which displays the path to our chosen file
-    #     self.lbl = QtGui.QLabel('No file selected')
-    #     self.vbox.addWidget(self.lbl)
-
-    #     # Create a push button labelled 'choose' and add it to our layout
-    #     btn = QtGui.QPushButton('Choose file', self)
-    #     self.vbox.addWidget(btn)
-        
-    #     # Connect the clicked signal to the get_fname handler
-    #     self.connect(btn, QtCore.SIGNAL('clicked()'), self.get_fname)
-
-    # def get_fname(self):
-    #     """
-    #     Handler called when 'choose file' is clicked
-    #     """
-    #     # When you call getOpenFileName, a file picker dialog is created
-    #     # and if the user selects a file, it's path is returned, and if not
-    #     # (ie, the user cancels the operation) None is returned
-    #     fname = QtGui.QFileDialog.getOpenFileName(self, 'Select file')
-    #     if fname:
-    #         self.lbl.setText(fname)
-    #     else:
-    #         self.lbl.setText('No file selected')
 
 
 
